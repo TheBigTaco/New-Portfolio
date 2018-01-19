03556b41b4574dba5b52cc95eb209941d2f5749b
Adam Titus <adamtitus76@gmail.com>
2018-01-19 14:57:41 -0800

    Add basic views and controller functionality for home and posts.

--- Portfolio/Controllers/PostController.cs
+++ Portfolio/Controllers/PostController.cs
@@ -4,21 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace Portfolio.Controllers
 {
-    public class HomeController : Controller
+    public class PostController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
         {
             return View();
         }
