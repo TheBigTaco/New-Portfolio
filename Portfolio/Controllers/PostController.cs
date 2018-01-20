f85cdb35aa963d20b875a65ac60d4d6e18f82f1c
Adam Titus <adamtitus76@gmail.com>
2018-01-19 16:50:38 -0800

    Add non-working ajax.

--- Portfolio/Controllers/PostController.cs
+++ Portfolio/Controllers/PostController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Portfolio.Models;
+using Portfolio.ViewModels;
+using Microsoft.AspNetCore.Identity;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Portfolio.Controllers
 {
     public class PostController : Controller
     {
-        // GET: /<controller>/
+        private readonly PortfolioDbContext _db;
+        private readonly UserManager<PortfolioUser> _userManager;
+        private readonly SignInManager<PortfolioUser> _signInManager;
+
+        public PostController(UserManager<PortfolioUser> userManager, SignInManager<PortfolioUser> signInManager, PortfolioDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreatePost()
         {
             return View();
         }
